10ac2f8af8d391d01a922af2245f4fa3b636390b
Jade-Isimbi <j.isimbituz1@alustudent.com>
2024-07-03 13:09:02 +0200

    t0

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -6,9 +6,16 @@ A simple script to fetch and print the number of subscribers for a given subredd
 
 import requests
 
-""" returns the number of subscribers """
-
 def sub_counter(sub_reddit):
+    """
+    Fetches the number of subscribers for a given subreddit.
+
+    Args:
+        sub_reddit (str): The name of the subreddit.
+
+    Returns:
+        int: The number of subscribers or 0 if an error occurs.
+    """
     USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"
     URL = f"https://api.reddit.com/r/{sub_reddit}/about"
     try:
