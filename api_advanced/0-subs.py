08b6df829996b5fdf5c6f9fa6d3331dcf840cd7e
Jade-Isimbi <j.isimbituz1@alustudent.com>
2024-07-03 13:16:42 +0200

    t0

---
+++ api_advanced/0-subs.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""
+A module to fetch and return the number of subscribers for a given subreddit.
+"""
+
+import requests
+
+def number_of_subscribers(subreddit):
+    """
+    Fetches the number of subscribers for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        int: The number of subscribers or 0 if an error occurs.
+    """
+    USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"
+    URL = f"https://api.reddit.com/r/{subreddit}/about"
+    try:
+        response = requests.get(URL, allow_redirects=False, headers={'User-Agent': USER_AGENT}).json()
+        return response.get("data", {}).get("subscribers", 0)
+    except Exception:
+        return 0
+
