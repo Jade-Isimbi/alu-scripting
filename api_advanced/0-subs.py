a2056c5b1d641688c1fda6f9bf03ca7e90261de7
Jade-Isimbi <j.isimbituz1@alustudent.com>
2024-07-03 11:44:12 +0200

    t1

---
+++ api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import requests
+
+""" returns the number of subscribers """
+
+def sub_counter(sub_reddit):
+    USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"
+    URL = f"https://api.reddit.com/r/{sub_reddit}/about"
+
+    response = requests.get(URL, allow_redirects=False, headers={'User-Agent': USER_AGENT}).json()
+    subscriber_count = response['data']['subscribers']
+
+    print(subscriber_count)
+
+sub_counter('liminalspace')
