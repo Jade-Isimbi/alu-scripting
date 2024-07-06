66125655691cab1104d8f67d0c1438cea0da336a
Jade-Isimbi <j.isimbituz1@alustudent.com>
2024-07-06 14:29:22 +0200

    t0

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -18,7 +18,11 @@ def number_of_subscribers(subreddit):
         or 0 if the subreddit is invalid.
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36'}
+    headers = {
+        'User-Agent': ('Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                       'AppleWebKit/537.36 (KHTML, like Gecko) '
+                       'Chrome/126.0.0.0 Safari/537.36')
+    }
 
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
@@ -33,4 +37,5 @@ if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Please pass an argument for the subreddit to search.")
     else:
-         print("{:d}".format(number_of_subscribers(sys.argv[1])))
+        print("{:d}".format(number_of_subscribers(sys.argv[1])))
+
