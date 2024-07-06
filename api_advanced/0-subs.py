acb874326f3257865199586ed5e7cc8ea99ec54a
Jade-Isimbi <j.isimbituz1@alustudent.com>
2024-07-06 14:56:15 +0200

    t0

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,41 +1,16 @@
 #!/usr/bin/python3
-"""
-This module contains a function that queries the Reddit API and
-returns the number of subscribers for a given subreddit.
-"""
+"""Script that fetch 10 hot post for a given subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Queries the Reddit API for the number of subscribers for a given subreddit.
-
-    Args:
-        subreddit (str): The subreddit to query.
-
-    Returns:
-        int: The number of subscribers for the subreddit,
-        or 0 if the subreddit is invalid.
-    """
+    """Return the number of subscribers for the given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        'User-Agent': ('Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
-                       'AppleWebKit/537.36 (KHTML, like Gecko) '
-                       'Chrome/126.0.0.0 Safari/537.36')
-    }
+    headers = {'User-Agent': 'My User Agent 1.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        return response.json().get('data', {}).get('subscribers', 0)
-    except Exception:
-        return 0
-
-
-if __name__ == "__main__":
-    import sys
-
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        print("{:d}".format(number_of_subscribers(sys.argv[1])))
-
+    if response.status_code == 200:
+        data = response.json().get('data')
+        if data:
+            return data.get('subscribers', 0)
+    return 0i
