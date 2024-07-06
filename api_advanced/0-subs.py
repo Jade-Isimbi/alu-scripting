46a79e39bb92933ab65906d56a7db7337eb95bfa
Jade-Isimbi <j.isimbituz1@alustudent.com>
2024-07-06 13:13:15 +0200

    t0

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 """
-A module to fetch and return the number of subscribers for a given subreddit.
+This module contains a function that queries the Reddit API and
+returns the number of subscribers for a given subreddit.
 """
 
 import requests
