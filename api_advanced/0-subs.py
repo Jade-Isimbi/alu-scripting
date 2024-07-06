2811bc3a1ce4b47cb24e7c8e6483197a4fed03c9
Jade-Isimbi <j.isimbituz1@alustudent.com>
2024-07-06 13:27:30 +0200

    t0

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -18,7 +18,11 @@ def number_of_subscribers(subreddit):
         or 0 if the subreddit is invalid.
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36'}
+    headers = {
+        'User-Agent': ('Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                       'AppleWebKit/537.36 (KHTML, like Gecko) '
+                       'Chrome/126.0.0.0 Safari/537.36')
+    }
 
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
